b0c580b86fffe5cd0a03e90c228e91941e61e662
DouBa <viphsj@gmail.com>
2018-01-21 23:08:35 +0800

    Delete aa.py

--- aa.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from bs4 import BeautifulSoup
-
-soup = BeautifulSoup(open("1.html"),"lxml")
-scroll_list = BeautifulSoup(str(soup.find("div", id="main")), "lxml")
-link = scroll_list.find_all("a")
-print(link)
