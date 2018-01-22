4916b19e999fa2aebabde588b524cd05bf97337d
DouBa <viphsj@gmail.com>
2018-01-22 11:06:26 +0800

    Update GetFund.py

--- GetFund.py
+++ GetFund.py
@@ -1,17 +1,56 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-' a test app '
+#######################################################################################################
+# 基金估值：http://fundgz.1234567.com.cn/js/160222.js?rt=1516587368315
+# 基金详细信息：http://fund.eastmoney.com/pingzhongdata/160222.js?v=20180122095601
+# 代码借鉴了以下文章：http://blog.csdn.net/yuzhucu/article/details/55261024
+#######################################################################################################
 
 __author__ = 'DouBa'
 
-import os
-import shutil
-from urllib import request
-from bs4 import BeautifulSoup
-from docx import Document
-from docx.shared import Pt
-from docx.enum.text import WD_ALIGN_PARAGRAPH
+import requests  
+from bs4 import BeautifulSoup  
+import time  
+import random  
+import pymysql  
+import os  
+import pandas as pd  
+import re
+
+def randHeader():  
+    # 随机生成User-Agent     
+    head_connection = ['Keep-Alive', 'close']  
+    head_accept = ['text/html, application/xhtml+xml,application/xml, */*']  
+    head_accept_language = ['zh-CN,fr-FR;q=0.5', 'en-US,en;q=0.8,zh-Hans-CN;q=0.5,zh-Hans;q=0.3']  
+    head_user_agent = ['Opera/8.0 (Macintosh; PPC Mac OS X; U; en)',  
+                       'Opera/9.27 (Windows NT 5.2; U; zh-cn)',  
+                       'Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.1; Win64; x64; Trident/4.0)',  
+                       'Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.1; Trident/4.0)',  
+                       'Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.1; WOW64; Trident/6.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0; InfoPath.2; .NET4.0C; .NET4.0E)',  
+                       'Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.1; WOW64; Trident/6.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0; InfoPath.2; .NET4.0C; .NET4.0E; QQBrowser/7.3.9825.400)',  
+                       'Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.1; WOW64; Trident/6.0; BIDUBrowser 2.x)',  
+                       'Mozilla/5.0 (Windows; U; Windows NT 5.1) Gecko/20070309 Firefox/2.0.0.3',  
+                       'Mozilla/5.0 (Windows; U; Windows NT 5.1) Gecko/20070803 Firefox/1.5.0.12',  
+                       'Mozilla/5.0 (Windows; U; Windows NT 5.2) Gecko/2008070208 Firefox/3.0.1',  
+                       'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.12) Gecko/20080219 Firefox/2.0.0.12 Navigator/9.0.0.6',  
+                       'Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/28.0.1500.95 Safari/537.36',  
+                       'Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0; .NET4.0C; rv:11.0) like Gecko)',  
+                       'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:21.0) Gecko/20100101 Firefox/21.0 ',  
+                       'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Maxthon/4.0.6.2000 Chrome/26.0.1410.43 Safari/537.1 ',  
+                       'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/21.0.1180.92 Safari/537.1 LBBROWSER',  
+                       'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.75 Safari/537.36',  
+                       'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.11 (KHTML, like Gecko) Chrome/20.0.1132.11 TaoBrowser/3.0 Safari/536.11',  
+                       'Mozilla/5.0 (Windows NT 6.3; WOW64; Trident/7.0; rv:11.0) like Gecko',  
+                       'Mozilla/5.0 (Macintosh; PPC Mac OS X; U; en) Opera 8.0'  
+                       ]  
+    result = {  
+        'Connection': head_connection[0],  
+        'Accept': head_accept[0],  
+        'Accept-Language': head_accept_language[1],  
+        'User-Agent': head_user_agent[random.randrange(0, len(head_user_agent))]  
+    }  
+    return result  
 
 def get_html_soup(url):#获取解编码后的HTML
     html = None
@@ -89,25 +128,6 @@ def clean_chinese_character(text):
         else:
             new_text += "_"
     return new_text;
-
-def create_docx(news_type, title, content):
-    '''这里使用python-docx库将新闻的内容生成word文件'''
-    document = Document()
-    paragraph = document.add_paragraph(title)
-    paragraph.alignment = WD_ALIGN_PARAGRAPH.CENTER
-    paragraph.bold = True
-
-    for x in content:
-        paragraph = document.add_paragraph(x)
-
-    style = paragraph.style
-    font = style.font
-    font.size = Pt(15)
-    font.name = "consolas"
-
-    name = news_type + "-" + clean_chinese_character(title) + ".docx"
-    document.save(news_type + "/" + name)
-
 ########################################################################
 
 code_url = "http://fund.eastmoney.com/160222.html"
